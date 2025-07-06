43b203522b653b58a7c8666327c6933dc66a4868
Christopher Agraz <c.agraz25@gmail.com>
2025-07-06 13:11:53 -0700

    add median function

--- git_branch_example.py
+++ git_branch_example.py
@@ -1,1 +1,12 @@
-print("hello_world");
+import math
+def median (input_list):
+  sorted_list = sorted(input_list)
+  n = len(sorted_list)
+  if n %2 == 1:
+    return sorted_list[n//2]
+  else:
+    mid1 = sorted_list[n//2-1]
+    mid2 = sorted_list[n//2]
+    return (mid1 + mid2)/2
+
+print(median(my_list))
